078db138575f42cf972f9cdc848df1f39a0297dc
Hoorulain Asad <hoorulainasad583@gmail.com>
2024-12-22 21:33:59 +0500

    Update ProductListing.js

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -3,12 +3,12 @@ import './styles.css';
 
 // Sample Product Data
 const products = [
-  { id: 1, name: 'Snake Plant', price: 15, category: 'Air Purifying', image: 'public/images/snake.jpeg' },
-  { id: 2, name: 'Spider Plant', price: 12, category: 'Air Purifying', image: 'public/images/spider.jpg' },
-  { id: 3, name: 'Peace Lily', price: 18, category: 'Aromatic', image: 'public/images/peace.jpeg' },
-  { id: 4, name: 'Lavender', price: 25, category: 'Aromatic', image: 'public/images/lavender-shk-1.jpg' },
-  { id: 5, name: 'Rosemary', price: 30, category: 'Scented', image: 'public/images/rosemary.jpg' },
-  { id: 6, name: 'Jasmine', price: 28, category: 'Scented', image: 'public/images/lily.jpeg' },
+  { id: 1, name: 'Snake Plant', price: 15, category: 'Air Purifying', image: 'images/snake.jpeg' },
+  { id: 2, name: 'Spider Plant', price: 12, category: 'Air Purifying', image: images/spider.jpg' },
+  { id: 3, name: 'Peace Lily', price: 18, category: 'Aromatic', image: 'images/peace.jpeg' },
+  { id: 4, name: 'Lavender', price: 25, category: 'Aromatic', image: 'images/lavender-shk-1.jpg' },
+  { id: 5, name: 'Rosemary', price: 30, category: 'Scented', image: 'images/rosemary.jpg' },
+  { id: 6, name: 'Jasmine', price: 28, category: 'Scented', image: 'images/lily.jpeg' },
 ];
 
 const ProductListing = ({ cart, addToCart }) => {
